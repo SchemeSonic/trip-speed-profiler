eb1701ecf1897d19a9902d97ed61263b661bf7a7
Haldun Yildiz <haldun313@gmail.com>
2020-04-17 10:06:22 +0300

    Updates

---
+++ precache-manifest.952c474890dee30d6e291987f08731dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b9dde4017cb11ee61e2f81757cda64",
+    "url": "/speed-profiler/index.html"
+  },
+  {
+    "revision": "b4d91fcba2644f0f5ea7",
+    "url": "/speed-profiler/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "ea492a514f1f6ac26657",
+    "url": "/speed-profiler/static/js/2.83895cf7.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/speed-profiler/static/js/2.83895cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d91fcba2644f0f5ea7",
+    "url": "/speed-profiler/static/js/main.688d305a.chunk.js"
+  },
+  {
+    "revision": "185cbc5d109fc158bb39",
+    "url": "/speed-profiler/static/js/runtime-main.e5ec7c02.js"
+  }
+]);
